f711d08a8b1fcd75a2c7b174d913449bcba17643
Michael McLarnon <michael@stellaservice.com>
2015-06-29 15:47:19 -0400

    switch to fsm-based logic for collecting tones

--- readMorse.js
+++ readMorse.js
@@ -1,33 +1,67 @@
 readMorse = {
-    init: function() {
-        readMorse.started = false;
-        document.onkeydown = document.onkeyup = readMorse.recordEvent;
-        readMorse.timings = { 'down': [], 'up': [] };
+    DOT_CUTOFF: 200,
+    LETTER_CUTOFF: 1000,
+
+    tones: [],
+    letters: [],
+    words: [],
+
+    states: {
+        outLetterUp: {
+            name: 'outLetterUp',
+            events: {
+                down: function() {
+                    audio.start();
+                    readMorse.moveState(readMorse.states.inLetterDown);
+                }
+            }
+        },
+        inLetterDown: {
+            name: 'inLetterDown',
+            events: {
+                up: function() {
+                    readMorse.tones.push(readMorse.timeSinceLast() < readMorse.DOT_CUTOFF ? '.' : '-');
+                    audio.end();
+                    readMorse.moveState(readMorse.states.inLetterUp);
+                }
+            }
+        },
+        inLetterUp: {
+            name: 'inLetterUp',
+            events: {
+                down: function() {
+                    audio.start();
+                    if (readMorse.timeSinceLast() > readMorse.LETTER_CUTOFF) {
+                        readMorse.letters.push(readMorse.decipher(readMorse.tones));
+                        readMorse.tones = [];
+                    }
+                    readMorse.moveState(readMorse.states.inLetterDown);
+                }
+            }
+        }
     },
 
-    recordEvent: function(e) {
-        var eventTime = Date.now();
-        if (e.keyCode == 16) {
-            if (e.type == 'keydown') {
-                if (!readMorse.startTime) {
-                    readMorse.startTime = eventTime;
-                }
-                readMorse.timings.down.push(eventTime);
-                audio.start();
-            }
-            else {
-                readMorse.timings.up.push(eventTime);
-                audio.end();
-            }
-            clearTimeout(readMorse.timer);
-            readMorse.timer = setTimeout(function() {
-                //morse.read(readMorse.timings);
-                document.write('<p>down: ' + readMorse.timings.down);
-                document.write('<br>up: ' + readMorse.timings.up);
-                document.onkeydown = document.onkeyup = readMorse.recordEvent;
-            }, 1000);
-        }
+    init: function() {
+        this.moveState(this.states.outLetterUp);
+    },
+
+    moveState: function(newState) {
+        console.log('Moving to ' + newState.name + '.');
+        console.log('Tones: ' + readMorse.tones);
+        console.log('Letters: ' + readMorse.letters);
+        readMorse.currentState = newState;
+        document.onkeydown = this.currentState.events.down;
+        document.onkeyup = this.currentState.events.up;
+        readMorse.timeMark = new Date().getTime();
+    },
+
+    timeSinceLast: function() {
+        return new Date().getTime() - readMorse.timeMark;
+    },
+
+    decipher: function(tones) {
+        return morse.lettersByCode[tones.join('')];
     }
-};
+}
 
 readMorse.init();
